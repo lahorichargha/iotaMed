92edca17e60b62da8b9b632b4b2f4881d84ec8ea
Martin Wehlou <martin@mitm.se>
2011-08-11 20:19:35 +0200

    Fix for activity indicator bug
    Activity indicator crashes under iOS 4.3, so in this workaround I added
    a category on UIImage that implements a dud initWithCoder: function. The
    bug appears in latest Xcode for Lion.

---
+++ iotaPad6/UIImage+HackyBugfix.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+HackyBugfix.h
+//  iotaPad6
+//
+//  Created by Martin Wehlou on 2011-08-11.
+//  Copyright (c) 2011 MITM AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (HackyBugfix)
+- (id)initWithCoder:(NSCoder *)aDecoder;
+@end
---
+++ iotaPad6/UIImage+HackyBugfix.m
@@ -0,0 +1,18 @@
+//
+//  UIImage+HackyBugfix.m
+//  iotaPad6
+//
+//  Created by Martin Wehlou on 2011-08-11.
+//  Copyright (c) 2011 MITM AB. All rights reserved.
+//
+
+#import "UIImage+HackyBugfix.h"
+
+@implementation UIImage (HackyBugfix)
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    return nil;
+}
+
+
+@end
